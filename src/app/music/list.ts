3535f00af12339c5652cff4ade403f7a51b66d3d
AmisKwok <58239518+AmisKwok@users.noreply.github.com>
2026-02-12 05:55:22 +0800

    Update list.ts

--- src/app/music/list.ts
+++ src/app/music/list.ts
@@ -1,6 +1,6 @@
 export const list = [
 	{
-		name: '我的悲伤是水做的',
-		iframe: `<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115259171935419&bvid=BV1HrJ9zXEvF&cid=32603177515&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>`
+		name: 'By my side',
+		iframe: `<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=115938934529625&bvid=BV1znBfBtEkM&cid=35547447797&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>`
 	}
 ]
